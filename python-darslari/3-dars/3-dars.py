32824b1a99dcf9ad87038343ebcd9745421438e3
Abdufattokhov <abdufattoxovazizbek13@gmail.com>
2022-12-25 15:54:09 +0400

    Darslar
    Darslarim yuklandi

---
+++ python-darslari/3-dars/3-dars.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 16:03:06 2022
+
+@author: Администратор
+"""
+
+#ism="Anvar"
+
+#shahar="Toshkent"
+#viloyat="Samarqand"
+
+#matn = "men yangi 🚙 sotib oldim"
+#smile = "😂"
+#print(smile)
+#print(matn)
+
+#ism = "Azizbek"
+#print("Mening ismim " + ism)
+
+#ism = "Ahad"
+#familya = "Qayum"
+#print(ism+familya)
+#print(ism+" "+familya)
+
+
+#print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas")
+
+#print("5 ning 4-darajasi", 5 ** 4)
+
+#print("Tomonlari 125 gateng kvadrat yuzi", 125 ** 125, "Perimetri", 4 * 125)
+
+
+#ism =  "Azizbek"
+#familya = "Abdufattoxov"
+#ism_sharif = f"Salamaleykum mening ismim",{familya}, {ism}, {familya}
+#print(ism_sharif)
+
+kocha = "Bog'gbon"
+mahalla = "Sog'bon"
+tuman = "Bodomzor"
+viloyat = "Samarqand"
+
+#oxirgi = input(f"{kocha} ko'chasi,  {mahalla} mahallasi,  {tuman} tumani,  {viloyat} viloyati")
+oxirgi = input("ko'changizni kirting \n>>>")
+oxirgi = input("mahallangizni kirting \n>>>" )
+oxirgi = input("tumaningizni kirting \n>>>" )
+oxirgi = input("viloyatingizni kirting \n>>>" )
+
+keyingi = f"{oxirgi}"
+print(keyingi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
