d751e16ba63a06db4bb3bda7f143b3d62922046b
Abdufattokhov <abdufattoxovazizbek13@gmail.com>
2022-12-25 16:04:40 +0400

    Yangi darslar
    Bu boshlang'ich darslar

---
+++ python-darslari/yngi dars.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  3 15:24:47 2022
+
+@author: Администратор
+"""
+
+ 1-dars print() funksiyasi
+
+
+print('Hayirli tong!')
+
+
+print('Men "Del" Markasidagi Noutbuk sotib oldim')
+
+
+#Beck slash "\" xar qanday belgidan oldin qoyilsa boladi
+print("Men \"del\"markasidan noutbuk sotib oldim")
+
+# """ asd """ matini 2ga boladi
+print("""Men yangi noutbuk sotib olmoqchiman,
+      uni qyerdan olay. """)
+      
+      
+# back slashning ikkinchi usuli "\n" manashu
+print("Men yangi noutbuk sotib olmoqchiman \nuni qayerdan olay") 
+
+#Arifmetik amallar 
+print(4+2*3)
+print(14/7)
+print(14//5)
+#Daraja o'lchagich " ** "
+print(5**6)
+
+print("To'qizning kavadrati", 9**2 , "ga teng") 
+
+print("3*3=", 3*3)
+
+
+
+#AMALYOT
+
+print('"Nexia", "Tico", \'Damas\' Korganlar qilar xavas')
+
+print("5ning 4 chi darajasi", 5**4)
+
+print("22ni 4ga bolganda qancha qoldiq qoladi", 22%4)
+
+print("Tomonlari 1125 ga teng kvadrat yuzi", 125*125, "premetiri", 15625-125 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
